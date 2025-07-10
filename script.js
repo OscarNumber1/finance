bb5a74674655b32a82262f445b1187fd411b885e
Vo Thanh Tuyen <thanhtuyen66202@gmail.com>
2025-07-11 01:14:01 +0700

    add some finance package

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const partners = [
     benefits: [
       "✔️ Vay tiền chỉ cần CCCD",
       "✔️ Không chứng minh thu nhập, không thẩm định người thân",
-      "✔️ Nhận ngay khoản vay chỉ trong 15 phút",
       "✔️ Lãi suất thấp nhất trên thị trường vay tín chấp",
+      "✔️ Nhận ngay khoản vay chỉ trong 15 phút",
       "✔️ Mức giải ngân đa dạng từ 3 - 10 triệu, dành cho mọi đối tượng",
     ],
     link: "https://shorten.asia/W2t1tsFY",
@@ -41,7 +41,7 @@ const partners = [
       },
       {
         img: "images/tnex/Note.png",
-        text: "<strong>Lưu ý</strong> <p>1. Khách hàng có địa chỉ thường trú trên CMT/CCCD không nằm trong các tỉnh/thành phố sau: Hải Phòng, Sóc Trăng, Quảng Ninh, Bến Tre, Tây Ninh, Đắk Nông, Đắk Lắk, Gia Lai, Cà Mau, Điện Biên, Lai Châu, Sơn La, Tuyên Quang, Kon Tum, Yên Bái</p><p>2. Có nhu cầu vay thật và thu nhập từ 8 triệu/tháng trở lên.</p><p>3. Khách hàng có list danh bạ thật, không được để trống danh bạ và cho phép truy cập vào danh bạ khi đăng ký vay.</p>",
+        text: "<strong>Lưu ý</strong> <p>1. Khách hàng có địa chỉ thường trú trên CMT/CCCD <strong>không nằm</strong> trong các tỉnh/thành phố sau: Hải Phòng, Sóc Trăng, Quảng Ninh, Bến Tre, Tây Ninh, Đắk Nông, Đắk Lắk, Gia Lai, Cà Mau, Điện Biên, Lai Châu, Sơn La, Tuyên Quang, Kon Tum, Yên Bái</p><p>2. Có nhu cầu vay thật và thu nhập từ 8 triệu/tháng trở lên.</p><p>3. Khách hàng có list danh bạ thật, không được để trống danh bạ và cho phép truy cập vào danh bạ khi đăng ký vay.</p>",
       },
     ],
   },
@@ -87,7 +87,7 @@ const partners = [
       },
       {
         img: "images/tnex/Note.png",
-        text: "<strong>Lưu ý</strong> <p>1. Khách hàng có địa chỉ thường trú trên CMT/CCCD không nằm trong các tỉnh/thành phố sau: Hải Phòng, Sóc Trăng, Quảng Ninh, Bến Tre, Tây Ninh, Đắk Nông, Đắk Lắk, Gia Lai, Cà Mau, Điện Biên, Lai Châu, Sơn La, Tuyên Quang, Kon Tum, Yên Bái</p><p>2. Có nhu cầu vay thật và thu nhập từ 8 triệu/tháng trở lên.</p><p>3. Khách hàng có list danh bạ thật, không được để trống danh bạ và cho phép truy cập vào danh bạ khi đăng ký vay.</p>",
+        text: "<strong>Lưu ý</strong> <p>1. Khách hàng có địa chỉ thường trú trên CMT/CCCD <strong>không nằm</strong> trong các tỉnh/thành phố sau: Hải Phòng, Sóc Trăng, Quảng Ninh, Bến Tre, Tây Ninh, Đắk Nông, Đắk Lắk, Gia Lai, Cà Mau, Điện Biên, Lai Châu, Sơn La, Tuyên Quang, Kon Tum, Yên Bái</p><p>2. Có nhu cầu vay thật và thu nhập từ 8 triệu/tháng trở lên.</p><p>3. Khách hàng có list danh bạ thật, không được để trống danh bạ và cho phép truy cập vào danh bạ khi đăng ký vay.</p>",
       },
     ],
   },
@@ -96,12 +96,12 @@ const partners = [
     logo: "images/lotte/logo.webp",
     benefits: [
       "✔️ Vay tiền chỉ cần CCCD",
-      "✔️ Hạn mức vay cao lên đến 500tr",
-      "✔️ Thời hạn vay dài từ 6-60 tháng",
-      "✔️ Giải ngân nhanh, trong vòng 4h với các hồ sơ đạt yêu cầu",
       "✔️ Ưu tiên cán bộ công chức, viên chức, dân văn phòng, công nhân",
+      "✔️ Thời hạn vay dài từ 6 - 60 tháng",
+      "✔️ Giải ngân nhanh, trong vòng 4h với các hồ sơ đạt yêu cầu",
+      "✔️ Hạn mức vay cao lên đến 500tr",
     ],
-    link: "https://shorten.asia/gXkkUphu",
+    link: "https://shorten.asia/eCyvcfS1",
     guideSteps: [
       {
         img: "images/lotte/Step1.png",
@@ -109,7 +109,89 @@ const partners = [
       },
       {
         img: "images/util/note.png",
-        text: "<strong>Lưu ý</strong> <p>1. Khách hàng có độ tuổi từ 25 tới 60 tuổi, có thu nhập trên 7 triệu/tháng, có hợp đồng lao động chính thức từ 1 năm và thời gian làm việc từ 3 năm trở lên</p><p>2. Ưu tiên Khách hàng là cán bộ công chức, viên chức, dân văn phòng, công nhân và có nhu cầu vay từ 10 tới 500 triệu đồng trong thời hạn từ 6 tới 60 tháng</p><p>3. Ưu tiên: khách hàng nữ, có thu nhập trên 3 triệu/tháng, có nhu cầu vay từ 10 tới 100 triệu trong thời hạn từ 6 tới 36 tháng.</p>",
+        text: "<strong>Lưu ý</strong> <p>1. Khách hàng có độ tuổi từ 25 tới 60 tuổi, có thu nhập trên 7 triệu/tháng, có hợp đồng lao động chính thức từ 1 năm và thời gian làm việc từ 3 năm trở lên.</p><p>2. Ưu tiên Khách hàng là cán bộ công chức, viên chức, dân văn phòng, công nhân và có nhu cầu vay từ 10 tới 500 triệu đồng trong thời hạn từ 6 tới 60 tháng.</p><p>3. Ưu tiên: khách hàng nữ, có thu nhập trên 3 triệu/tháng, có nhu cầu vay từ 10 tới 100 triệu trong thời hạn từ 6 tới 36 tháng.</p>",
+      },
+    ],
+  },
+  {
+    name: "FECREDIT",
+    logo: "images/fecredit/logo.png",
+    benefits: [
+      "✔️ Vay tiền chỉ cần CCCD",
+      "✔️ Thủ tục đơn giản, không thế chấp",
+      "✔️ Thời hạn vay dài từ 6 - 60 tháng",
+      "✔️ Tỷ lệ duyệt vay cao",
+      "✔️ Gói vay từ 10 triệu – 100 triệu đồng",
+    ],
+    link: "#",
+    guideSteps: [
+      {
+        img: "images/fecredit/Step1.png",
+        text: "Khách hàng điền đầy đủ thông tin sau đó ấn <strong>Gửi</strong> và nhớ chú ý điện thoại trong vòng 15 phút để được tư vấn.",
+      },
+      {
+        img: "images/util/note.png",
+        text: "<strong>Lưu ý</strong> <p>1. Khách hàng có độ tuổi từ 25 tới 60 tuổi, có thu nhập trên 4 triệu/tháng, ưu tiên khách hàng cũ sẽ được giải ngân cao hơn.</p><p>2. Khách được vay song song với khoản vay mua xe, mua điện thoại.</p><p>3. Nhu cầu vay từ 10 triệu trở lên (Nhu cầu vay thấp hơn sẽ không được tư vấn tiếp).</p><p>4. Sau khi có màn hình thông báo Đăng ký hồ sơ vay thành công chiến dịch FECredit, khách hàng không nên tắt trình duyệt ngay. <strong>Nên đợi 1 - 2 phút để Website load đầy đủ trang.</strong></p>",
+      },
+    ],
+  },
+  {
+    name: "HOMECREDIT",
+    logo: "images/homecredit/logo.png",
+    benefits: [
+      "✔️ Vay tiền chỉ cần CCCD",
+      "✔️ Không chứng minh thu nhập, không thẩm định người thân",
+      "✔️ Thời hạn vay dài từ 6 - 57 tháng",
+      "✔️ Quy trình xét duyệt hồ sơ và giải ngân nhanh nhất chỉ trong 10 phút",
+      "✔️ Hỗ trợ vay từ 5 - 200 triệu",
+    ],
+    link: "https://shorten.asia/aerSAjcw",
+    guideSteps: [
+      {
+        img: "images/homecredit/Step1.png",
+        text: "<span>Bước 1:</span> Chọn <strong>vay tiền online nhanh</strong> sau đó ấn <strong>Vay ngay</strong>.",
+      },
+      {
+        img: "images/homecredit/Step2.png",
+        text: "<span>Bước 2:</span> Chọn khoảng vay.",
+      },
+      {
+        img: "images/homecredit/Step3.png",
+        text: "<span>Bước 3:</span> Nhập thông tin cá nhân.",
+      },
+      {
+        img: "images/homecredit/Step4.png",
+        text: "<span>Bước 4:</span> Nhập mã OTP được gửi về số điện thoại.",
+      },
+      {
+        img: "images/homecredit/Step5.png",
+        text: "<span>Bước 5:</span> Màn hình như sau là đã <strong>vay thành công</strong>.",
+      },
+      {
+        img: "images/util/Note.png",
+        text: "<strong>Lưu ý</strong> <p>1. Khách hàng có địa chỉ thường trú ở miền Nam và các thành phố lớn của miền Bắc.</p><p>2. Khách hàng có thu nhập từ 7.5 - 24tr mỗi tháng trở lên.</p><p>3. Khách hàng có độ tuổi từ 25 - 45 tuổi và đã tốt nghiệp cấp 3.</p>",
+      },
+    ],
+  },
+  {
+    name: "Tima",
+    logo: "images/tima/logo.png",
+    benefits: [
+      "✔️ Vay tiền chỉ cần CCCD và giấy tờ xe (xe máy hoặc ô tô)",
+      "✔️ Cam kết không giữ tài sản đảm bảo",
+      "✔️ Thời hạn vay từ 9 - 12 tháng",
+      "✔️ Quy trình online 99%, hỗ trợ tận nơi",
+      "✔️ Hạn mức vay cao từ 3 - 500tr",
+    ],
+    link: "https://shorten.asia/Hjask8Xk",
+    guideSteps: [
+      {
+        img: "images/tima/Step1.png",
+        text: "Khách hàng điền đầy đủ thông tin sau đó ấn <strong>Nhận tiền ngay</strong>.",
+      },
+      {
+        img: "images/util/note.png",
+        text: "<strong>Lưu ý</strong> <p>1. Khách hàng là công dân Việt Nam trong độ tuổi 18 – 65 tuổi tính đến thời điểm tất toán khoản vay.</p><p>2. Đang sinh sống và làm việc thường xuyên tại khu vực: Hà Nội, Sài Gòn, Bình Dương, Đồng Nai, Hưng Yên, Bắc Ninh, Bắc Giang, Vĩnh Phúc, Hải Dương, Thanh Hoá, Nghệ An.</p><p>3. Có bản chụp ảnh gốc CMND/thẻ căn cước/hộ chiếu và có bản chụp ảnh gốc sổ hộ khẩu.</p><p>4. Khách hàng có hoặc đang sử dụng xe ô tô theo giấy tờ của đăng ký/ cà vẹt xe ô tô và có đăng ký xe ô tô và giấy đăng kiểm.</p>",
       },
     ],
   },
